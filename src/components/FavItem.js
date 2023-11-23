570369740070fa4c6171e77d89039bb24b6bbb3c
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-23 19:30:05 +0300

    corrects title prop

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -2,15 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { removeFav } from "../actions";
 
-function FavItem({ fact, id }) {
+function FavItem({ title, id }) {
   const dispatch = useDispatch();
+
   const handleRemove = () => {
     dispatch(removeFav(id));
   };
 
   return (
     <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
-      <div className="flex-1 pr-4">{fact}</div>
+      <div className="flex-1 pr-4">{title}</div>
       <button
         onClick={handleRemove}
         className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"

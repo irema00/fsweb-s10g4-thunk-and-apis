cd5576a11cab9a710852fd021ca03e203c45a4b3
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-23 16:51:10 +0300

    moves import to top

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,10 @@
+import axios from "axios";
 export const GET_FAVS_FROM_LS = "GET_FAVS_FROM_LS";
 export const FAV_ADD = "FAV_ADD";
 export const FAV_REMOVE = "FAV_REMOVE";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_LOADING = "FETCH_LOADING";
 export const FETCH_ERROR = "FETCH_ERROR";
-
-import axios from "axios";
 
 export const getFavsFromLocalStorage = () => {
   return { type: GET_FAVS_FROM_LS };

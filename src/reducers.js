0cec2b067cad164c6955d2029d66a24e88943310
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-23 18:36:51 +0300

    completes reducer conditions

--- src/reducers.js
+++ src/reducers.js
@@ -25,22 +25,25 @@ function readFavsFromLocalStorage() {
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+      return { ...state, favs: [...state.favs, action.payload] };
 
     case FAV_REMOVE:
-      return state;
+      return {
+        ...state,
+        favs: state.favs.filter((fav) => fav.id !== action.payload),
+      };
 
     case FETCH_SUCCESS:
-      return state;
+      return { ...state, current: action.payload, loading: false };
 
     case FETCH_LOADING:
-      return state;
+      return { ...state, loading: true };
 
     case FETCH_ERROR:
-      return state;
+      return { ...state, error: action.payload, loading: false };
 
     case GET_FAVS_FROM_LS:
-      return state;
+      return { ...state, favs: readFavsFromLocalStorage() };
 
     default:
       return state;
